137c03598a0ae7d9f1f2457e8572a5e919ca4439
kuochunchang <guojun.tw@gmail.com>
2016-03-09 16:12:44 +0800

    bug fix

---
+++ emmet-sales-pi/src/main/java/emmet/sales/pi/exception/DataNotFoundException.java
@@ -0,0 +1,12 @@
+package emmet.sales.pi.exception;
+
+public class DataNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+
+	public DataNotFoundException(String msg){
+		super(msg);
+	}
+
+}
