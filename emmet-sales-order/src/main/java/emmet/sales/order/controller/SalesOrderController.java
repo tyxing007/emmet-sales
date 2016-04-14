52d2d5d1eb393a95550bed13101cef4bd806d6a5
louie <elventer@hotmail.com>
2016-04-14 16:12:40 +0800

    no message

--- emmet-sales-order/src/main/java/emmet/sales/order/controller/SalesOrderController.java
+++ emmet-sales-order/src/main/java/emmet/sales/order/controller/SalesOrderController.java
@@ -1,15 +1,18 @@
 package emmet.sales.order.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import emmet.core.data.entity.Order;
 import emmet.sales.order.model.SalesOrderCreateModel;
+import emmet.sales.order.repository.SalesOrderRepsitory;
 import emmet.sales.order.service.SalesOrderService;
 
 @RepositoryRestController()
@@ -19,6 +22,8 @@ public class SalesOrderController {
 
 	@Autowired
 	SalesOrderService salesOrderService;
+	@Autowired
+	SalesOrderRepsitory salesOrderRepsitory;
 	
 	@RequestMapping(value = "/createFromPI", method = RequestMethod.POST)
 	public ResponseEntity<?> createOrderFromPI(@RequestBody SalesOrderCreateModel model){
@@ -33,6 +38,21 @@ public class SalesOrderController {
 		
 			
 		return ResponseEntity.ok(order);
+		
+	}
+	
+	@RequestMapping(value = "/search/findByIdLike", method = RequestMethod.GET)
+	public ResponseEntity<?> createOrderFromPI(@RequestParam("id") String id ,Pageable page){
+		
+		
+		try {
+			return ResponseEntity.ok(salesOrderRepsitory.findByIdLike(id, page));
+		} catch (Exception e) {
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		
+			
+		
 		
 	}
 
