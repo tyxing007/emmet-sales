2a5e908bf0aa3342cd8e11abc5ad79059bf59134
kuochunchang <guojun.tw@gmail.com>
2016-06-24 10:21:51 +0800

    remove consul client

--- emmet-sales-shipment/src/main/java/emmet/sales/shipment/SalesShipmantApplication.java
+++ emmet-sales-shipment/src/main/java/emmet/sales/shipment/SalesShipmantApplication.java
@@ -3,9 +3,7 @@ package emmet.sales.shipment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @EntityScan({"emmet.sales.entity.order"})
 public class SalesShipmantApplication {
